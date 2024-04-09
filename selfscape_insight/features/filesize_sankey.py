39bf2dc80f4818fbdce943f2f4dd600b5307539f
Noah DE <71356818+duggann4@users.noreply.github.com>
2024-04-09 09:48:12 -0700

    Write docs and cleanup boilerplate

--- selfscape_insight/features/filesize_sankey.py
+++ selfscape_insight/features/filesize_sankey.py
@@ -12,32 +12,31 @@ Example usage:
     >>> fsk.run(json_path, out_path)
     filepath to HTML page containing results of analysis
 
-    $ python3 filesize_sankey.py -json_path /path/to/jsons/ -out_path /path/to/output.html
-    filepath to HTML page containing results of analysis
+    $ python3 filesize_sankey.py -json_path /path/to/jsons/ -out_path /path/to/output
+    filepath to where to write filesize_sankey.html page containing results of analysis
 
 Dependencies:
     pandas for data handling
-    seaborn for datavis
-    [ADD DESCRIPTIONS FOR FURTHER THIRD-PARTY IMPORTS HERE]
-
+    plotly for datavis
 
 Note:
     This sub-module is part of the 'selfscape_insight' package in the 'features' module.
 
 Todo:
-    * Write docstrings
     * Add mimetypes-based color coding
     * Refactor helpers into separate module in core
     * Prettify the output
 
 Version:
-    1.0
+    1.0.1
 
 Author:
     Noah Duggan Erickson
 """
 
 # CHANGELOG:
+#   1.0.1: (09 April 2024)
+#     - Added docstrings & comments
 #   1.0: (05 April 2024)
 #     - Initial Release
 #     - (transfer from notebook to script)
@@ -45,26 +44,46 @@ Author:
 import argparse
 import os
 # import mimetypes
-# Add your other built-in imports here
 
 import pandas as pd
 import plotly.graph_objects as go
-# Add your other third-party/external imports here
-# Please update requirements.txt as needed!
 
 def pdEnumFiles(rootPath):
+    ''' Enumerates files in a root directory and returns as a pandas dataframe
+
+    Enumerates files in a root directory, with `inbox` directories excluded,
+    and returns as a pandas dataframe with columns for root, file, and path.
+
+    Args:
+        rootPath (str): The path to the root directory to enumerate
+    
+    Returns:
+        pd.DataFrame: A pandas dataframe with columns for root, file, and path
+        for each file in the root directory
+    '''
     fileList = []
     rootList = []
     pathList = []
     for root, dirs, files in os.walk(rootPath):
         for file in files:
-            if (not 'messages' in root):
+            if (not 'inbox' in root):
                 fileList.append(file)
                 rootList.append(root)
                 pathList.append(f"{root}/{file}")
     return pd.DataFrame({'root': rootList, 'file': fileList, 'path':pathList})
 
 def dEnumDirs(rootPath):
+    ''' Generates ID values for directories in a root directory
+
+    Walks through a root directory and maps sequential ID values
+    to each subdirectory via a dictionary.
+
+    Args:
+        rootPath (str): The path to the root directory to enumerate
+    
+    Returns:
+        dict: A dictionary mapping each subdirectory path to a unique ID value
+    '''
     pathDict = {'-':0}
     pathDict[rootPath] = 1
     counter = 2
@@ -75,6 +94,17 @@ def dEnumDirs(rootPath):
     return pathDict
 
 def pdEnumDirs(rootPath):
+    ''' Enumerates directories in a root directory and returns a dataframe
+
+    Enumerates directories in a root directory and returns a pandas
+    dataframe with columns for root, dir, and path.
+    
+    Args:
+        rootPath (str): The path to the root directory to enumerate
+
+    Returns:
+        pd.DataFrame: A pandas dataframe with columns root, dir, and path
+    '''
     dirList = [rootPath]
     rootList = ['-']
     pathList = [rootPath]
@@ -86,47 +116,64 @@ def pdEnumDirs(rootPath):
     return pd.DataFrame({'root': rootList, 'dir': dirList, 'path':pathList})
 
 def run(json_path:str, out_path:str):
-    # TODO: Please refer to sample.py for run() docstring format!
-    print("Running the filesize_sankey feature module")
-
+    # Enumerate files and directories, get filesizes
+    #
     fdf = pdEnumFiles(json_path)
     fdf['size'] = fdf.apply(lambda x: os.stat(x['path']).st_size, axis=1)
     pdict = dEnumDirs(json_path)
     ddf = pdEnumDirs(json_path)
+
+    # Map directories to parent directories via IDs
+    #
     ddf['didx'] = ddf['path'].map(pdict)
     ddf['pidx'] = ddf['root'].map(pdict)
+
+    # Assign files to directory IDs
     ddf.index = ddf['path']
     ddf.drop(['root', 'dir', 'path'], axis=1, inplace=True)
     fdf = fdf.join(ddf, on='root')
     ddf['path'] = ddf.index
     ddf.index = ddf['didx']
+
+    # Aggregate file sizes by directory
+    #
     ddf['size'] = fdf.groupby('didx').sum()['size']
     ddf = ddf.fillna(0)
-    for i in range(25):
+    for i in range(25): # Arbitrary number of iterations to ensure all directories are accounted for
         tsr = ddf.groupby('pidx').sum()['size']
         for j, v in tsr.items():
             ddf.at[j, 'size'] = v
     ddf = ddf.dropna()
+
+    # Build Sankey diagram connections
+    #
     tddf = pd.DataFrame({'source': ddf['pidx'], 'target': list(ddf.index), 'value': ddf['size'], 'label':ddf['path'].apply((lambda x: f"{x}.dir"))})
     tddf.index = list(range(len(tddf)))
     tddf['label'] = tddf['label'].apply((lambda x: x.split('/')[-1]))
-    fdf['fidx'] = [i+ddf['didx'].max()+1 for i in list(range(len(fdf)))]
+    fdf['fidx'] = [i+ddf['didx'].max()+1 for i in list(range(len(fdf)))] # Assign file IDs
     tfdf = pd.DataFrame({'source': fdf['didx'], 'target': fdf['fidx'], 'value': fdf['size'], 'label':fdf['file']})
     tfdf.index = list(range(len(tfdf)))
     bdf = pd.concat([tddf, tfdf], ignore_index=True)
+
     tls = pd.Series(list(bdf['label']), index = list(bdf['target']))
     tls[0] = 'root'
     tls = tls.sort_index()
+
+    # Assign colors based on file extension
+    # TODO: Replace dict with mimetype-based color coding
+    #
     tldf = pd.DataFrame({'label':tls.apply((lambda x: x.split('.')[0])), 'ext':tls.apply((lambda x: x.split('.')[-1]))})
     colorDict = {'root': 'black', 'dir': 'purple', 'json': 'blue', 'txt': 'white', 'jpg': 'orange', 'mp4':'red'}
     tldf['colors'] = tldf['ext'].map(colorDict)
+
+    # Generate Sankey diagram visuals
+    #
     node = dict(label = tldf['label'], color = tldf['colors'])
     link = dict(source = bdf['source'], target = bdf['target'], value = bdf['value'])
     data = go.Sankey(node = node, link = link)
     fig = go.Figure(data)
+
     fig.write_html(os.path.join(out_path, 'filesize_sankey.html'))
-
-
     return f"Wrote to {os.path.join(out_path, 'filesize_sankey.html')}"
 
 if __name__ == "__main__":
@@ -134,6 +181,6 @@ if __name__ == "__main__":
                                      description='A short description of what your code does')
     parser.add_argument('-json_path', metavar='path/to/jsons/',
                         help='path to json files', required=True)
-    parser.add_argument('-out_path', metavar='path/to/output.html')
+    parser.add_argument('-out_path', metavar='path/to/output(/filesize_sankey.html)',)
     args = parser.parse_args()
     print(run(args.json_path, args.out_path))
